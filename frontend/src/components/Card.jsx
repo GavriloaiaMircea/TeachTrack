98e7ad8959af4f473d8e27ed94301dd2e72e7f1f
GavriloaiaMircea <rowan.mtg@yahoo.com>
2024-09-25 16:34:48 +0300

    Refactor Card component to capitalize the first letter of the subject prop

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Card } from "react-bootstrap";
 
 function ClassCard(props) {
+  const subject =
+    props.subject[0].toUpperCase() + props.subject.slice(1).toLowerCase();
+
   return (
     <Card className="mb-4 shadow-sm">
       <Card.Body>
         <Card.Title>{props.class_name}</Card.Title>
-        <Card.Text>{props.subject}</Card.Text>
+        <Card.Text>{subject}</Card.Text>
         <Card.Text>{props.school_year}</Card.Text>
       </Card.Body>
     </Card>
