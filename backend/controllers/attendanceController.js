069a2022c543917e792ad4030b4c3dcf5cdc638f
GavriloaiaMircea <rowan.mtg@yahoo.com>
2024-09-26 16:36:50 +0300

    Refactor attendanceController to add functionality for adding attendance

--- backend/controllers/attendanceController.js
+++ backend/controllers/attendanceController.js
@@ -1,4 +1,5 @@
 import db from "../config/db.js";
+import { parse, format } from "date-fns";
 
 export const getAttendance = async (req, res) => {
   const student_id = req.params.student_id.trim();
@@ -46,5 +47,42 @@ export const delteAttendance = async (req, res) => {
   } catch (err) {
     console.log(err);
     return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const addAttendance = async (req, res) => {
+  const { date, status } = req.body;
+  const student_id = req.params.student_id.trim();
+  const class_id = req.params.class_id.trim();
+
+  try {
+    // Parsează data din formatul DD/MM/YYYY în YYYY-MM-DD
+    const parsedDate = parse(date, "dd/MM/yyyy", new Date());
+    const formattedDate = format(parsedDate, "yyyy-MM-dd");
+
+    const response = await db.query(
+      "SELECT id FROM students_classes WHERE student_id = $1 AND class_id = $2",
+      [student_id, class_id]
+    );
+
+    if (response.rowCount === 0) {
+      return res.status(400).json({ message: "Student not found in class" });
+    }
+
+    const result = await db.query(
+      "INSERT INTO attendance (date, status, student_class_id) VALUES ($1, $2, $3) RETURNING *",
+      [formattedDate, status, response.rows[0].id]
+    );
+
+    if (result.rowCount === 0) {
+      return res.status(400).json({ message: "Attendance not added" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Attendance added", attendance: result.rows[0] });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
--- backend/routes/attendanceRoutes.js
+++ backend/routes/attendanceRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import {
   delteAttendance,
   getAttendance,
+  addAttendance,
 } from "../controllers/attendanceController.js";
 
 const router = express.Router();
@@ -9,5 +10,7 @@ const router = express.Router();
 router.get("/:student_id/:class_id", getAttendance);
 
 router.delete("/:id", delteAttendance);
+
+router.post("/:student_id/:class_id", addAttendance);
 
 export default router;
