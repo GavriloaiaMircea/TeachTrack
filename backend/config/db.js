4e1b97c912dce15b0e6e7012386d30b670aecf83
GavriloaiaMircea <rowan.mtg@yahoo.com>
2024-09-27 14:25:46 +0300

    Refactor database configuration to use environment variable for connection string and enable SSL

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,12 +4,12 @@ import pg from "pg";
 dotenv.config();
 
 const db = new pg.Client({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
 db.connect();
 
 export default db;
